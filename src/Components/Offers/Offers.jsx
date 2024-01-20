c44e4cb11f56260715d79c1ff22295e1d653793e
Nawodi_Priyawansha <n.t.priyawansha@gmail.com>
2024-01-20 23:54:13 +0530

    added offers in home page and adjust some alignments in home page

---
+++ src/Components/Offers/Offers.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import exclusive_image from '../Assests/exclusive_image.png'
+import './Offers.css'
+
+const Offers = () => {
+  return (
+    <div className='offers'>
+      <div className="offers-left">
+        <h1>Exclusive</h1>
+        <h1>Offers For You</h1>
+        <p>ONLY ON BEST SELLERS PRODUCT</p>
+        <button>Ckeck Now</button>
+      </div>
+      <div className="offers-right">
+        <img src={exclusive_image} alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Offers
