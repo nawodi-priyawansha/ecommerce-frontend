12aaca7f9aa59f6bf1471cc6725bf3efa3136686
Nawodi_Priyawansha <n.t.priyawansha@gmail.com>
2024-01-20 22:54:54 +0530

    added items in home page

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
+import Populer from '../Components/Populer/Populer'
 
 const Shop = () => {
   return (
     <div>
       <Hero></Hero>
+      <Populer/>
     </div>
   )
 }
