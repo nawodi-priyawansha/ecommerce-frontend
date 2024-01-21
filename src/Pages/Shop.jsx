33624b99b45302d46f9b98ce3bcec00e191844e9
Nawodi_Priyawansha <n.t.priyawansha@gmail.com>
2024-01-21 15:43:02 +0530

    added newcollection in home page

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import Populer from '../Components/Populer/Populer'
 import Offers from '../Components/Offers/Offers'
+import NewCollections from '../Components/NewCollection/NewCollections'
 
 const Shop = () => {
   return (
@@ -9,6 +10,7 @@ const Shop = () => {
       <Hero/>
       <Populer/>
       <Offers/>
+      <NewCollections/>
     </div>
   )
 }
