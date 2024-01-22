bf7882fbd5380cb94129133d396aac0ac50e5c25
Nawodi_Priyawansha <n.t.priyawansha@gmail.com>
2024-01-22 14:54:17 +0530

    added newsletter in the home page

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -3,6 +3,7 @@ import Hero from '../Components/Hero/Hero'
 import Populer from '../Components/Populer/Populer'
 import Offers from '../Components/Offers/Offers'
 import NewCollections from '../Components/NewCollection/NewCollections'
+import NewsLetter from '../Components/Navbar/NewsLetter/NewsLetter'
 
 const Shop = () => {
   return (
@@ -11,6 +12,7 @@ const Shop = () => {
       <Populer/>
       <Offers/>
       <NewCollections/>
+      <NewsLetter/>
     </div>
   )
 }
