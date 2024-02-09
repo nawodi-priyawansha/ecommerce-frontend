27660fe1783b3aaebd01a7402abead47f9fd3d19
Nawodi Thathsarani <119826598+nawodi-priyawansha@users.noreply.github.com>
2024-02-09 15:02:37 +0530

    login page ui

--- src/Pages/LoginSignu.jsx
+++ src/Pages/LoginSignu.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import './CSS/LoginSignu.css'
 
 const LoginSignu = () => {
   return (
-    <div>
-      
+    <div className='loginsignup'>
+      <div className="loginsigup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-feilds">
+          <input type="text" placeholder='Your Name'/>
+          <input type="email" placeholder='Email Adress' />
+          <input type="password" placeholder='Password' />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">Already have an account? <span>Login</span></p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name='' id='' />
+          <p>By continuing, i agree to the terms of use & privacy policy.</p>
+        </div>
+      </div>
     </div>
   )
 }
